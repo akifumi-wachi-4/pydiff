de4c4a85398632d3d0f6a84b3f626df4b2c77d71
Akifumi Wachi <53202549+akifumi-wachi-4@users.noreply.github.com>
2026-01-29 21:31:41 +0900

    Delete print function

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,6 @@
-// difff《ﾃﾞｭﾌﾌ》JavaScript functions
-
-function hideForm() {
-    const form = document.getElementById('form');
-    const top = document.getElementById('top');
-    const news = document.getElementById('news');
-    const hideBtn = document.getElementById('hide');
-    
-    if (form.style.display === 'none') {
-        // Show all elements
-        top.style.display = 'block';
-        form.style.display = 'block';
-        news.style.display = 'block';
-        hideBtn.value = '結果のみ表示 (印刷用)';
-    } else {
-        // Hide elements for print mode
-        top.style.display = 'none';
-        form.style.display = 'none';
-        news.style.display = 'none';
-        hideBtn.value = '全体を表示';
-    }
-}
+// Modern Diff Tool - JavaScript functions
 
 function setColor1() {
-    document.getElementById('top').style.borderTop = '5px solid #00BBFF';
     const emList = document.getElementsByTagName('em');
     for (let i = 0; i < emList.length; i++) {
         emList[i].className = 'blue';
@@ -30,7 +8,6 @@ function setColor1() {
 }
 
 function setColor2() {
-    document.getElementById('top').style.borderTop = '5px solid #00bb00';
     const emList = document.getElementsByTagName('em');
     for (let i = 0; i < emList.length; i++) {
         emList[i].className = 'green';
@@ -38,7 +15,6 @@ function setColor2() {
 }
 
 function setColor3() {
-    document.getElementById('top').style.borderTop = '5px solid black';
     const emList = document.getElementsByTagName('em');
     for (let i = 0; i < emList.length; i++) {
         emList[i].className = 'black';
